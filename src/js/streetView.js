88c826257e654e19826c2eb535c7284b57714e8d
ChiChou <zhouz@knownsec.com>
2015-03-11 22:55:00 +0800

    migrate from mapbox to leaflet
    fix coordinate errors
    remove mapbox lib
    add leaflet library

--- src/js/streetView.js
+++ src/js/streetView.js
@@ -1,6 +1,5 @@
 $(function() {
   // init mapbox
-  L.mapbox.accessToken = 'pk.eyJ1IjoiY29kZWNvbG9yaXN0IiwiYSI6Ik9qT0hDa1EifQ.jQqEGsy46F77OL0m7E6xIA';
 
   var transform = function(point) {
     var magic = Math.PI * 3000.0 / 180.0;
@@ -21,11 +20,26 @@ $(function() {
   southEast = transform(southEast);
 
   var bounds = L.latLngBounds(northWest, southEast);
-  var map = L.mapbox.map('map', 'codecolorist.le813p3d', {
-    // maxBounds: bounds,
+
+  var retina = function(tileSource) {
+    return window.devicePixelRatio > 1.5 ? tileSource.replace('.png', '@2x.png') : tileSource;
+  };
+
+  L.Icon.Default.imagePath = 'images/leaflet/';
+
+  var map = L.map('map', {
+    attributionControl: false,
     center: transform(center),
-    // minZoom: 15
+    // maxBounds: bounds,
+    zoom: 15,
+    minZoom: 14
   });
+
+  L.tileLayer(retina(
+    'http://{s}.tiles.mapbox.com/v4/codecolorist.le813p3d/{z}/{x}/{y}.png?' +
+    'access_token=pk.eyJ1IjoiY29kZWNvbG9yaXN0IiwiYSI6Ik9qT0hDa1EifQ.jQqEGsy46F77OL0m7E6xIA'), {
+    detectRetina: true
+  }).addTo(map);
 
   // show pins
   $.getJSON('data/scenes.json', function(data) {
@@ -43,7 +57,8 @@ $(function() {
       return '<li>' + innerHTML + '</li>';
     });
 
-    $('#gallery-list').html(html).on('click', 'a', function(e) {
+    $('#gallery-list').html(html);
+    $('.leaflet-popup-pane, #gallery-list').on('click', 'a.place', function(e) {
       e.preventDefault();
       loadScene(this.href);
     });
@@ -84,22 +99,27 @@ $(function() {
   renderer.setSize(w, h);
   renderer.domElement.style.position = 'absolute';
 
-  var controls = new DeviceOrientationController( camera, renderer.domElement );
-  // controls.rotateSpeed = -0.5;
-  // controls.zoomSpeed = 3;
-  // controls.minDistance = -radius;
-  // controls.maxDistance = radius >> 1;
+  var controls;
+  if ("onorientationchange" in window) {
+    // mobile devices
+    controls = new DeviceOrientationController(camera, renderer.domElement);
+    controls.connect();
+    camera.position.z = 10;
+  } else {
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    camera.position.z = -100;
+    controls.rotateSpeed = -1.0;
+    controls.minDistance = -500;
+    controls.maxDistance = 500;
+    window.camera = camera;
+  }
+
   controls.addEventListener('change', render);
-  controls.connect();
-
   container.appendChild(renderer.domElement);
-
-  camera.position.z = 10;
   animate();
 
   function animate() {
     controls.update();
-    
     // Todo: tween animation between scenes
     requestAnimationFrame(animate);
   }
@@ -117,22 +137,29 @@ $(function() {
 
     renderer.setSize(w, h);
     render();
-    
-  }, false);
 
+  }, false);
 
   function loadScene(src) {
     $('#map').fadeOut();
+    $('#gallery-list, #gallery-stage').addClass('off');
 
     var img = new Image();
     img.onload = function() {
-      var ratio = img.width / img.height;
+      var aspect = img.width / img.height;
 
       // var maxWidth = screen.width * 2;
-      canvas.width = Math.min(3200, (img.width | 1) - 1);
+      canvas.width = Math.min(screen.width * 2, (img.width | 1) - 1);
       canvas.height = canvas.width >> 1;
 
-      var h = (canvas.width / ratio) >> 0,
+      var polarAngle = Math.PI * 2 * aspect - camera.fov;
+      console.log(aspect, camera.fov);
+
+      controls.minPolarAngle = 0;
+      // controls.maxPolarAngle = polarAngle;
+      console.log(polarAngle);
+
+      var h = (canvas.width / aspect) >> 0,
         top = (canvas.height - h) >> 1;
 
       canvasContext.save();
@@ -141,9 +168,6 @@ $(function() {
       canvasContext.drawImage(img, 0, top, canvas.width, h);
       canvasContext.restore();
 
-      this.minPolarAngle = 0;
-      this.maxPolarAngle = Math.PI;
-
       texture.needsUpdate = true;
       render();
     }
@@ -151,4 +175,7 @@ $(function() {
 
   }
 
+  $('#gallery-stage').on('mouseenter', function() {
+    $('#gallery-stage, #gallery-list').removeClass('off');
+  });
 });
